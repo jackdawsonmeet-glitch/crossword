23e9632ddb9ab40fab98eb967303df787fee6f68
jackdawsonmeet-glitch <jackdawsonmeet@gmail.com>
2026-09-14 04:15:50 -0700

    Update ad-block-2-settings.js

--- dist/ad-block-2-settings.js
+++ dist/ad-block-2-settings.js
@@ -19,7 +19,7 @@
 window.AD_BLOCK_2 = {
   enabled: true,
   type: "animation", // "animation", "image", or "video"
-  destination: "https://sample2.com",
+  destination: "https://testingerror.netlify.app",
   mediaUrl: "",
   posterUrl: "",
   fit: "cover",
