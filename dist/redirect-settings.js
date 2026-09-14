37ae8a45e2aee5c9cfca51fba05b81de855a2611
jackdawsonmeet-glitch <jackdawsonmeet@gmail.com>
2026-09-14 13:54:30 -0700

    Update redirect-settings.js

--- dist/redirect-settings.js
+++ dist/redirect-settings.js
@@ -10,5 +10,5 @@
 window.GAME_REDIRECT = {
   enabled: true,
   delaySeconds: 600,
-  destination: "https://crosswordgameplay.netlify.app/"
+  destination: "https://www.wordgames.com/crossword/"
 };
