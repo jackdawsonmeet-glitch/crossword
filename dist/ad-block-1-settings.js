f2f15033890d1d9e0635078739ab19ff3ae2f272
jackdawsonmeet-glitch <jackdawsonmeet@gmail.com>
2026-09-14 13:53:29 -0700

    Update ad-block-1-settings.js

--- dist/ad-block-1-settings.js
+++ dist/ad-block-1-settings.js
@@ -9,7 +9,7 @@
 window.AD_BLOCK_1 = {
   enabled: true,
   type: "animation",
-  destination: "https://crosswordgameplay.netlify.app/",
+  destination: "https://www.wordgames.com/",
   mediaUrl: "",
   posterUrl: "",
   headline: "CLICK HERE",
